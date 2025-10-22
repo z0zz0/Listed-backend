911f33322702c587353f80f99754a9a0f4a0490e
z0zz0 <edlir_1996@live.com>
2025-10-23 01:40:39 +0200

    Fixed project structure. Added logging. Added CorrelationId to Logcontext through middleware. Fixed so app runs locally in docker container, through docker-compose and Dockerfile_local

---
+++ src/Listed.API/Middleware/CorrelationIdMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Serilog.Context;
+
+namespace Listed.API.Middleware;
+
+public class CorrelationIdMiddleware(RequestDelegate next)
+{
+    private const string HeaderName = "X-Correlation-ID";
+
+    public async Task Invoke(HttpContext context)
+    {
+        var headerValue = context.Request.Headers[HeaderName].FirstOrDefault();
+        string correlationId = Guid.TryParse(headerValue, out var parsedGuid) ? parsedGuid.ToString() : Guid.NewGuid().ToString();
+
+        context.Items[HeaderName] = correlationId;
+        context.Response.OnStarting(() =>
+        {
+            context.Response.Headers[HeaderName] = correlationId;
+            return Task.CompletedTask;
+        });
+
+        using (LogContext.PushProperty("CorrelationId", correlationId))
+        {
+            await next(context);
+        }
+    }
+}
--- src/Listed.API/Program.cs
+++ src/Listed.API/Program.cs
@@ -1,3 +1,6 @@
+using Listed.API.Middleware;
+using Serilog;
+
 namespace Listed.API;
 
 public class Program
@@ -5,9 +8,18 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Configuration)
+            .CreateLogger();
+
+        builder.Host.UseSerilog();
+
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        app.UseMiddleware<CorrelationIdMiddleware>();
+
+        app.MapGet("/", () => { Log.Information("Reached endpoint."); return "Hello World!"; });
 
         app.Run();
     }
